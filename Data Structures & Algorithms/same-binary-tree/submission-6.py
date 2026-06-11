a7a8409bbb40d499946241087d22b5d89ca3d148
Nayan15112001 <87423773+Nayan15112001@users.noreply.github.com>
2026-06-11 23:56:04 +0530

    Add: same-binary-tree - submission-6

---
+++ Data Structures & Algorithms/same-binary-tree/submission-6.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def dfs(p,q):
+            if not p and q:
+                return False
+            if p and not q:
+                return False
+            if not p and not q:
+                return True
+
+            if p.val != q.val:
+                return False
+            
+            dfs(p.left,q.left)
+            dfs(p.right,q.right)
+            return dfs(p.left, q.left) and dfs(p.right, q.right)
+        return dfs(p,q)
+
+
+
+
+            
