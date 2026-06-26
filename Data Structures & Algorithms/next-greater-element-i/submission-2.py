2acae129101b63d7e03eb1646206e58711322f49
Nayan15112001 <87423773+Nayan15112001@users.noreply.github.com>
2026-06-26 12:40:36 +0530

    Add: next-greater-element-i - submission-2

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stk = []
+        ans = []
+        hmap = {}
+        n2 = len(nums2)
+        for num in nums2:
+            if not stk:
+                stk.append(num)
+            elif num>stk[-1]:
+                while stk and num>stk[-1]:
+                    hmap[stk.pop()] = num
+                stk.append(num)
+            else:
+                stk.append(num)
+        print(stk)
+        for num in stk:
+            hmap[num] = -1
+
+        print(hmap)
+        for num in nums1:
+            ans.append(hmap[num])
+        return ans
+
