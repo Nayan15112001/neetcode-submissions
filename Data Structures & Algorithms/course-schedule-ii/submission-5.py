1f1bf2270178875849aaed8d28a9452d0004bf92
Nayan15112001 <87423773+Nayan15112001@users.noreply.github.com>
2026-07-12 23:38:07 +0530

    Add: course-schedule-ii - submission-5

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-5.py
@@ -0,0 +1,43 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        res = []
+        adj_list = {}
+        seen = set()
+        done = set()
+        #building the adj list
+        for i in range(numCourses):
+            adj_list[i] = []
+        for prereq in prerequisites:
+            adj_list[prereq[0]].append(prereq[1])
+
+        def dfs(k):
+            #base condition
+
+            if k in seen:
+                return False
+            if k in done:
+                return True
+            
+            seen.add(k)
+            for val in adj_list[k]:
+                if not dfs(val):
+                    return False
+            
+            seen.remove(k)
+            done.add(k)
+            res.append(k)
+
+            return True
+
+
+        for k,v in adj_list.items():
+            if not dfs(k):
+                return []
+
+        
+        # if len(done) == numCourses:
+        #     return done
+        # else:
+        #     return []
+
+        return res
