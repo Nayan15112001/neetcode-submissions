44ff79fbf60dc6e9d7a3463061cb8fe1d7be8ff4
Nayan15112001 <87423773+Nayan15112001@users.noreply.github.com>
2026-08-03 01:56:25 +0530

    Add: decode-ways - submission-9

---
+++ Data Structures & Algorithms/decode-ways/submission-9.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        dp  = {n:1}
+        if s[0] == '0':
+            return 0
+        def ways(x):
+            if x in dp:
+                return dp[x]
+            if x>=n:
+                return 0
+            
+            if s[x] == '0':
+                dp[x] = 0
+            else:
+                dp[x]=ways(x+1)
+                if x+1<n:
+                    if s[x] == '1' or (s[x] == '2' and s[x+1] in ("0123456")):
+                        dp[x]+=ways(x+2)
+            return dp[x]
+
+        return ways(0)
+            
