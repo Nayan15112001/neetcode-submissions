e358163f8f3537454db42085f022f6f25c1971f3
Nayan15112001 <87423773+Nayan15112001@users.noreply.github.com>
2026-08-17 12:54:52 +0530

    Add: interleaving-string - submission-1

---
+++ Data Structures & Algorithms/interleaving-string/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        n1 = len(s1)
+        n2 = len(s2)
+
+        dp = {}
+        
+        if n1+n2!=len(s3):
+            return False
+        def dfs(i1,i2):
+            if (i1,i2) in dp:
+                return dp[(i1,i2)]
+            
+            ans = False
+
+            if i1== n1 and i2== n2:
+                return True
+
+            if i1<=n1-1 and s1[i1] == s3[i1+i2] and i2<=n2-1 and s2[i2] == s3[i1+i2]:
+                ans = dfs(i1+1,i2) or dfs(i1,i2+1)
+
+            
+            elif i1<=n1-1 and s1[i1] == s3[i1+i2]:
+                ans = dfs(i1+1,i2)
+            
+            elif i2<=n2-1 and s2[i2] == s3[i1+i2]:
+                ans = dfs(i1,i2+1)
+            
+            dp[(i1,i2)] = ans
+            return dp[(i1,i2)]
+        
+        return dfs(0,0)
