21d8f783f91b4697d708bdda702e6f0050f4fb51
Nayan15112001 <87423773+Nayan15112001@users.noreply.github.com>
2026-08-19 10:47:45 +0530

    Add: count-subsequences - submission-2

---
+++ Data Structures & Algorithms/count-subsequences/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        i,j = 0,0
+        n1,n2 = len(s),len(t)
+        dp = {}
+
+        if n1<n2:
+            return 0
+
+        def dfs(i,j):
+            if (i,j) in dp:
+                return dp[(i,j)]
+            
+            if j == n2:
+                return 1
+            if i>=n1 or j>n2:
+                return 0
+            
+            if s[i] == t[j]:
+                dp[(i,j)] = dfs(i+1,j+1) + dfs(i+1,j)
+            
+            else:
+                dp[(i,j)] = dfs(i+1,j)
+
+            
+            return dp[(i,j)]
+        
+        return dfs(0,0)
+
+        
+
+
+
