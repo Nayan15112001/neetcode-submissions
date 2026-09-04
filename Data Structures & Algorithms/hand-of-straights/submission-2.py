ae4ef279ba173b4c3b0d30d7af75d6d4ecdfb1b6
Nayan15112001 <87423773+Nayan15112001@users.noreply.github.com>
2026-09-04 23:35:55 +0530

    Add: hand-of-straights - submission-2

---
+++ Data Structures & Algorithms/hand-of-straights/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        hand.sort()
+        n = len(hand)
+        count = Counter(hand)
+        cards  = sorted(count.keys())
+        for card in cards:
+            if count[card] == 0:
+                continue
+            freq = count[card]
+
+            for i in range(groupSize):
+                needed = card+i
+                if count[needed]<freq:
+                    return False
+                
+                count[needed]-=freq
+
+
+
+        return True
+            
